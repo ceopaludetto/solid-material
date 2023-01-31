e120ad117d85043f32499624865f9688a79790d3
Carlos Eduardo <ceo.paludetto@gmail.com>
2023-01-31 13:01:21 -0300

    fix: sidebar behavior on mobile

--- apps/docs/src/utils/click-outside.ts
+++ apps/docs/src/utils/click-outside.ts
@@ -1,17 +1,23 @@
-import { createSignal, onCleanup, onMount } from "solid-js";
+import type { Accessor } from "solid-js";
+
+import { onCleanup, onMount } from "solid-js";
 
 const events = ["mousedown", "touchstart"];
 
-export function createClickOutside<T extends HTMLElement>(callback: (el: T) => void) {
-  const [ref, getRef] = createSignal<T>();
+type CreateClickOutsideOptions<T extends HTMLElement> = {
+  ref: Accessor<T | undefined>;
+  isDisabled?: Accessor<boolean | undefined>;
+  callback: (el: T) => void;
+};
 
+export function createClickOutside<T extends HTMLElement>(options: CreateClickOutsideOptions<T>) {
   function onClick(event: Event) {
-    const el = ref();
-    if (el && !el.contains(event.target as HTMLElement)) callback(el);
+    if (options.isDisabled?.()) return;
+
+    const el = options.ref();
+    if (el && !el.contains(event.target as HTMLElement)) options.callback(el);
   }
 
   onMount(() => events.forEach((event) => document.addEventListener(event, onClick)));
   onCleanup(() => events.forEach((event) => document.removeEventListener(event, onClick)));
-
-  return getRef;
 }
